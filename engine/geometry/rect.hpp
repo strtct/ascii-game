ea6195fa478bcb2362220279f0cdff3fecceac6a
stratcat <stratogato@proton.me>
2025-05-21 22:51:06 +0100

    se elimina archivo duplicado

--- engine/geometry/rect.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-struct Rect {
-    int x, y, width, height;
-
-    bool contains(int px, int py) const {
-        return (px >= x && px < x + width && py >= y && py < y + height);
-    }
-};
